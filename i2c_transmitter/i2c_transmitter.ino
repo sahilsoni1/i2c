252722f159034ca9eb9a54c4914bcf1d7cd9453a
unknown <sahilsoni19970@gmail.com>
2019-05-20 12:05:24 +0530

    both code work well

---
+++ i2c_transmitter/i2c_transmitter.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+
+void setup() {
+ Serial.begin(9600); /* begin serial comm. */
+ Wire.begin(); /* join i2c bus as master */
+ Serial.println("I am I2C Master");
+}
+
+void loop() {
+ Wire.beginTransmission(8); /* begin with device address 8 */
+ Wire.write("Hello Slave");  /* sends hello string */
+ Wire.endTransmission();    /* stop transmitting */
+
+ Wire.requestFrom(8, 9); /* request & read data of size 9 from slave */
+ while(Wire.available()){
+    char c = Wire.read();/* read data received from slave */
+  Serial.print(c);
+ }
+ Serial.println();
+ delay(1000);
+}
